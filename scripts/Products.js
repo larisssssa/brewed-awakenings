1584b83ec5ba41797c649a8c5518f27480024372
Larissa Ferreira <larissadferr@gmail.com>
2025-12-01 15:39:46 -0600

    Products - added event listener for window alert to show product name and price when click product

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,12 +1,25 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (click) => {
+        const clicked = click.target
+
+        if (clicked.dataset.type === "product") {
+            window.alert(`
+                ${clicked.dataset.productname}
+                $${clicked.dataset.productprice}`)
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li data-type="product" data-productid="${product.id}">${product.name}</li>`
+        html += `<li data-type="product" data-productid="${product.id}" data-productname="${product.name}" data-productprice="${product.price}">${product.name}</li>`
     }
 
     html += "</ul>"
