a308c0bae34d55f04bfebf4a61604086b901d202
Larissa Ferreira <larissadferr@gmail.com>
2025-12-02 09:31:36 -0600

    Employees - event listener to show amount of products sold per employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,4 +1,19 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (click) => {
+        const clicked = click.target
+
+        if (clicked.dataset.type === "employee") {
+            const orders = getOrders()
+            const employeeOrders = orders.filter(order => order.employeeId === parseInt(clicked.dataset.employeeid))
+            window.alert(`${clicked.dataset.employeename} sold ${employeeOrders.length} product(s).`)
+        }
+    }
+)
+
+
 
 const employees = getEmployees()
 
@@ -6,7 +21,7 @@ export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li data-type="employee" data-employeeid="${employee.id}">${employee.name}</li>`
+        html += `<li data-type="employee" data-employeeid="${employee.id}" data-employeename="${employee.name}">${employee.name}</li>`
     }
 
     html += "</ul>"
